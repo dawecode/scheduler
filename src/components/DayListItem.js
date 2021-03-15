c0ae148614ceb57e2f9287901ab347f736d18491
Catherine Dawe <dawe.cm@gmail.com>
2021-03-15 17:59:50 -0400

    add new component DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "components/DayListItem.scss";
+
+const classNames = require('classnames');
+
+export default function DayListItem(props) {
+  const dayListClass = classNames ("day-list__item",{
+    dayListClass :true,
+    "day-list__item " :props.item,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  })
+  return (
+    <li 
+      className = {dayListClass}
+      onClick = {()=>props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light"> {props.spots ? props.spots : "no"} spots remaining</h3>
+    </li>
+  );
+}
